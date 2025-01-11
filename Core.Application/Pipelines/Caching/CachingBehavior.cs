2e21539072280dc9777a11fd862f2ade7dd6a93b
burakgunce <brk.gnc9@hotmail.com>
2025-01-11 23:29:22 +0300

    in memory cache build

--- Core.Application/Pipelines/Caching/CachingBehavior.cs
+++ Core.Application/Pipelines/Caching/CachingBehavior.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +15,13 @@ public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, 
 {
     private readonly CacheSettings _cacheSettings;
     private readonly IDistributedCache _cache;
+    private readonly ILogger<CachingBehavior<TRequest, TResponse>> _logger;
 
-    public CachingBehavior(CacheSettings cacheSettings, IDistributedCache cache)
+    public CachingBehavior(IDistributedCache cache, ILogger<CachingBehavior<TRequest, TResponse>> logger, IConfiguration configuration)
     {
-        _cacheSettings = cacheSettings;
+        _cacheSettings = configuration.GetSection("CacheSettings").Get<CacheSettings>()?? throw new InvalidOperationException();
         _cache = cache;
+        _logger = logger;
     }
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
@@ -32,6 +36,7 @@ public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, 
         if (cachedResponse != null)
         {
             response = JsonSerializer.Deserialize<TResponse>(Encoding.Default.GetString(cachedResponse));
+            _logger.LogInformation($"Fetched from Cache -> {request.CacheKey}");
         }
         else
         {
@@ -51,6 +56,7 @@ public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, 
         byte[] serializedData = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response));
 
         await _cache.SetAsync(request.CacheKey, serializedData, cacheOptions, cancellationToken);
+        _logger.LogInformation($"Added to Cache -> {request.CacheKey}");
 
         return response;
     }
